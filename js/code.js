9e07eb590b0e7d60fbad6f46b94d3401d779cb4f
marcocarneiro <marco.carneiro7@gmail.com>
2021-11-25 12:47:41 -0300

    Implementação de código para consumo de API do IBGE

--- js/code.js
+++ js/code.js
@@ -1,2 +1,24 @@
 // Inicializa a biblioteca de animação AOS
-AOS.init(); 
+AOS.init();
+
+
+//FUNÇÃO ENCAPSULADA: Preenche o SELECT com os dados da API do IBGE
+(function(){
+    var url = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados';
+    var select = document.getElementById('estado');
+    
+    /* Consulta a API com o método fetch e 
+    com um laço de repetição incrementa as tags option */
+    //1o then() captura os dados, 2o then() trata os dados, catch() tratamento do erro
+    fetch(url).then(response => response.json()).then(json => {        
+        var options = '<option>Selecione</option>';
+        //DESAFIO: inserir abaixo desta linha um código para ordenar
+        //em ordem alfabética o objeto json
+        
+        //laço de repetição
+        for (let index = 0; index < json.length; index++) {
+            options += '<option>'+json[index].nome+'</option>';
+        }
+        select.innerHTML = options;
+    }).catch(erro => console.log(erro));
+})();
